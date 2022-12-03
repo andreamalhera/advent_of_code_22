fd8eeb908f812414d34cb5dc101b1e402aa581a2
Andrea Maldonado <>
2022-12-03 13:57:31 +0100

    Day 3 first solution

---
+++ 3.py
@@ -0,0 +1,37 @@
+"""Solution to Day 3 https://adventofcode.com/2022/day/3"""
+
+# Author: Andrea Maldonado andreamalher.works@gmail.com
+# License: MIT License
+
+import pandas as pd
+
+letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+        "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+def advent_3_1(df):
+    df[['comp_1', 'comp_2']] = df.apply(lambda x: [x['rucksack'][:int(len(x['rucksack'])/2)],
+        x['rucksack'][int(len(x['rucksack'])/2):]], axis =1, result_type = "expand")
+    df['intersection']= df.apply(lambda x: set(x['comp_1']).intersection(set(x['comp_2'])).pop(), axis=1)
+    df['prio'] = df.apply(lambda x: letters.index(x['intersection'])+1, axis =1)
+    print("1*: The sum of priorities of those items is", sum(df["prio"]), ".")
+
+def advent_3_2(df):
+    rucksacks = df["rucksack"]
+    for x in rucksacks[::3]:
+
+
+def test_3_1():
+    rucksacks = ["vJrwpWtwJgWrhcsFMMfFFhFp","jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL","PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn","ttgJtRGJQctTZtZT","CrZsJsPPZsGzwwsLwLmpwMDw"]
+    rucksacks = pd.DataFrame(rucksacks, columns=['rucksack'])
+    advent_3_1(rucksacks)
+    advent_3_2(rucksacks)
+
+if __name__ == '__main__':
+    print("Running tests using website examples:")
+    test_3_1()
+
+    INPUT_PATH = "input/3.txt"
+    print("Running using ", INPUT_PATH)
+    df = pd.read_csv(INPUT_PATH, sep=' ', header=None, names=['rucksack'])
+    advent_3_1(df)
