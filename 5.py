ec07d8b85f766b26fc3779a198049f9fac2dba63
Andrea Maldonado <>
2022-12-07 15:10:27 +0100

    Day 5 solution reads stacks from file

--- 5.py
+++ 5.py
@@ -1,4 +1,24 @@
 import re
+def start_stacks(lines):
+    n = 4
+    moves=[]
+    temp = []
+    lst = []
+    board = []
+    for line in lines:
+        if line.startswith("move"):
+            moves.append(line)
+        elif bool(re.search(r'\d', line)):
+            count_stacks = re.findall(r'\d+', line)
+        else:
+            chunks = [line[i:i+n].replace("[","").replace("]","")
+                    .replace("\n","").replace(" ","") for i in range(0, len(line), n)]
+            temp.append(chunks)
+    temp = temp[:-1]
+    board = list(zip(*temp[::-1]))
+    board = [list(filter(None, list(row))) for row in board]
+    return board, moves
+
 def advent5_1(start, moves):
     result = []
     for line in moves:
@@ -10,7 +30,7 @@ def advent5_1(start, moves):
             item = start[source-1].pop()
             start[target-1].append(item)
     result.append([column.pop() for column in start])
-    print("1*: After all rouds the crates at the top are:", "".join(result[0]))
+    print("1*: After all rounds the crates at the top are:", "".join(result[0]))
 
 def advent5_2(start, moves):
     result = []
@@ -26,13 +46,14 @@ def advent5_2(start, moves):
     print("2*: After all rouds the crates at the top are:", "".join(result[0]))
 
 def test5():
-    start = [["Z","N"],["M","C","D"],["P"]]
-    moves = "move 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2"
-    lines = moves.split("\n")
+    TEST_PATH = "input/test5.txt"
+    f = open(TEST_PATH, "r")
+    lines = f.readlines()
+    start, moves = start_stacks(lines)
+    advent5_1(start, moves)
 
-    advent5_1(start, lines)
-    start = [["Z","N"],["M","C","D"],["P"]]
-    advent5_2(start, lines)
+    start, moves = start_stacks(lines)
+    advent5_2(start, moves)
 
 if __name__ == '__main__':
     print("Running tests using website examples:")
@@ -42,12 +63,8 @@ if __name__ == '__main__':
     print("Running using", INPUT_PATH)
     f = open(INPUT_PATH, "r")
     lines = f.readlines()
-    board = [["N","B","D","T","V","G","Z","J"],["S","R","M","D","W","P","F"],["V","C","R","S","Z"],
-            ["R","T","J","Z","P","H","G"],["T","C","J","N","D","Z","Q","F"],["N","V","P","W","G","S","F","M"],
-            ["G","C","V","B","P","Q"],["Z","B","P","N"],["W","P","J"]]
-    moves = lines[10:]
+    board, moves = start_stacks(lines)
     advent5_1(board, moves)
-    board = [["N","B","D","T","V","G","Z","J"],["S","R","M","D","W","P","F"],["V","C","R","S","Z"],
-            ["R","T","J","Z","P","H","G"],["T","C","J","N","D","Z","Q","F"],["N","V","P","W","G","S","F","M"],
-            ["G","C","V","B","P","Q"],["Z","B","P","N"],["W","P","J"]]
+
+    board, moves = start_stacks(lines)
     advent5_2(board, moves)
